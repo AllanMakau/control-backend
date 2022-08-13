aa7315d66e6cb7ec8ad6bf8a9dee7596b1f58f30
de Jesus Lima <andi@gft.com>
2022-08-12 21:56:31 -0300

    testando build validation

--- src/main/java/br/com/mksoftware/control/ControlApplication.java
+++ src/main/java/br/com/mksoftware/control/ControlApplication.java
@@ -8,7 +8,7 @@ public class ControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControlApplication.class, args);
-		System.out.println("Iniciando 12");
+		System.out.println("Iniciando 125");
 	}
 
 }
